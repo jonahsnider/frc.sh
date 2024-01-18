eac25598f52fd4bf651dfded5be89e526306581b
Jonah Snider <jonah@jonahsnider.com>
2024-01-17 22:39:46 -0800

    refactor: use TileGroup component to reduce duplicated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { CategoryId } from '@/content';
-import { Tile } from './tile';
+import { TileGroup } from './tile-group';
 import { TileHeading } from './tile-heading';
-import { TileSectionHeading } from './tile-section-heading';
 
 // biome-ignore lint/style/noDefaultExport: This has to be a default export
 export default function Home() {
@@ -26,144 +25,81 @@ export default function Home() {
 				</div>
 			</TileHeading>
 
-			<TileSectionHeading className='lg:col-start-1 bg-ctre bg-opacity-15'>CTR Electronics</TileSectionHeading>
-			<Tile
-				className='lg:col-start-1'
+			<TileGroup
 				category={CategoryId.Ctre}
-				name='Docs'
-				url='https://pro.docs.ctr-electronics.com/en/latest/'
-			/>
-			<Tile
 				className='lg:col-start-1'
-				category={CategoryId.Ctre}
-				name='Examples'
-				url='https://github.com/CrossTheRoadElec/Phoenix6-Examples'
-			/>
-			<Tile
-				className='lg:col-start-1'
-				category={CategoryId.Ctre}
-				name='Phoenix Javadoc'
-				url='https://api.ctr-electronics.com/phoenix6/release/java/'
-			/>
-			<Tile
-				className='lg:col-start-1'
-				category={CategoryId.Ctre}
-				name='Phoenix C++ reference'
-				url='https://api.ctr-electronics.com/phoenix6/release/cpp/'
-			/>
-			<Tile
-				className='lg:col-start-1'
-				category={CategoryId.Ctre}
-				name='Phoenix Python reference'
-				url='https://api.ctr-electronics.com/phoenix6/release/python/'
+				title='CTR Electronics'
+				tiles={[
+					{ name: 'Docs', url: 'https://pro.docs.ctr-electronics.com/en/latest/' },
+					{ name: 'Examples', url: 'https://github.com/CrossTheRoadElec/Phoenix6-Examples' },
+					{ name: 'Phoenix Javadoc', url: 'https://api.ctr-electronics.com/phoenix6/release/java/' },
+					{ name: 'Phoenix C++ reference', url: 'https://api.ctr-electronics.com/phoenix6/release/cpp/' },
+					{ name: 'Phoenix Python reference', url: 'https://api.ctr-electronics.com/phoenix6/release/python/' },
+				]}
 			/>
 
-			<TileSectionHeading className='bg-rev bg-opacity-15 lg:col-start-2'>REV</TileSectionHeading>
-			<Tile
-				className='lg:col-start-2'
+			<TileGroup
 				category={CategoryId.Rev}
-				name='Docs'
-				url='https://docs.revrobotics.com/brushless'
-			/>
-			<Tile
 				className='lg:col-start-2'
-				category={CategoryId.Rev}
-				name='Examples'
-				url='https://github.com/REVrobotics/SPARK-MAX-Examples/tree/master'
-			/>
-			<Tile
-				className='lg:col-start-2'
-				category={CategoryId.Rev}
-				name='REVLib Javadoc'
-				url='https://codedocs.revrobotics.com/java/com/revrobotics/package-summary.html'
-			/>
-			<Tile
-				className='lg:col-start-2'
-				category={CategoryId.Rev}
-				name='REVLib C++ reference'
-				url='https://codedocs.revrobotics.com/cpp/namespacerev.html'
-			/>
-			<Tile
-				className='lg:col-start-2'
-				category={CategoryId.Rev}
-				name='REVLib Python reference'
-				url='https://robotpy.readthedocs.io/projects/rev/en/stable/rev.html'
+				title='REV'
+				tiles={[
+					{ name: 'Docs', url: 'https://docs.revrobotics.com/brushless' },
+					{ name: 'Examples', url: 'https://github.com/REVrobotics/SPARK-MAX-Examples/tree/master' },
+					{ name: 'REVLib Javadoc', url: 'https://codedocs.revrobotics.com/java/com/revrobotics/package-summary.html' },
+					{ name: 'REVLib C++ reference', url: 'https://codedocs.revrobotics.com/cpp/namespacerev.html' },
+					{ name: 'REVLib Python reference', url: 'https://robotpy.readthedocs.io/projects/rev/en/stable/rev.html' },
+				]}
 			/>
 
-			<TileSectionHeading className='bg-wpilib bg-opacity-15 lg:col-start-3'>WPILib</TileSectionHeading>
-			<Tile
-				className='lg:col-start-3'
+			<TileGroup
 				category={CategoryId.Wpilib}
-				name='Docs'
-				url='https://docs.wpilib.org/en/stable/'
-			/>
-			<Tile
 				className='lg:col-start-3'
-				category={CategoryId.Wpilib}
-				name='Javadoc'
-				url='https://github.wpilib.org/allwpilib/docs/release/java/index.html'
-			/>
-			<Tile
-				className='lg:col-start-3'
-				category={CategoryId.Wpilib}
-				name='C++ reference'
-				url='https://github.wpilib.org/allwpilib/docs/release/cpp/index.html'
-			/>
-			<Tile
-				className='lg:col-start-3'
-				category={CategoryId.Wpilib}
-				name='Python reference'
-				url='https://robotpy.readthedocs.io/projects/robotpy/en/stable/'
-			/>
-			<Tile
-				className='lg:col-start-3'
-				category={CategoryId.Wpilib}
-				name='Java examples'
-				url='https://github.com/wpilibsuite/allwpilib/tree/main/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples'
-			/>
-			<Tile
-				className='lg:col-start-3'
-				category={CategoryId.Wpilib}
-				name='C++ examples'
-				url='https://github.com/wpilibsuite/allwpilib/tree/main/wpilibcExamples/src/main/cpp/examples'
+				title='WPILib'
+				tiles={[
+					{ name: 'Docs', url: 'https://docs.wpilib.org/en/stable/' },
+					{ name: 'Javadoc', url: 'https://github.wpilib.org/allwpilib/docs/release/java/index.html' },
+					{ name: 'C++ reference', url: 'https://github.wpilib.org/allwpilib/docs/release/cpp/index.html' },
+					{ name: 'Python reference', url: 'https://robotpy.readthedocs.io/projects/robotpy/en/stable/' },
+					{
+						name: 'Java examples',
+						url: 'https://github.com/wpilibsuite/allwpilib/tree/main/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples',
+					},
+					{
+						name: 'C++ examples',
+						url: 'https://github.com/wpilibsuite/allwpilib/tree/main/wpilibcExamples/src/main/cpp/examples',
+					},
+				]}
 			/>
 
-			<TileSectionHeading className='bg-advantagekit bg-opacity-15 lg:col-start-2'>AdvantageKit</TileSectionHeading>
-			<Tile
-				className='lg:col-start-2'
+			<TileGroup
 				category={CategoryId.Advantagekit}
-				name='Docs'
-				url='https://github.com/Mechanical-Advantage/AdvantageKit#readme'
-			/>
-
-			<TileSectionHeading className='bg-pathplanner bg-opacity-15 lg:col-start-1'>PathPlanner</TileSectionHeading>
-			<Tile
-				className='lg:col-start-1'
-				category={CategoryId.Pathplanner}
-				name='Docs'
-				url='https://pathplanner.dev/home.html'
-			/>
-			<Tile
-				className='lg:col-start-1'
-				category={CategoryId.Pathplanner}
-				name='Examples'
-				url='https://github.com/mjansen4857/pathplanner/tree/main/examples'
-			/>
-
-			<TileSectionHeading className='bg-photonvision bg-opacity-15 lg:col-start-2'>Photonvision</TileSectionHeading>
-			<Tile
 				className='lg:col-start-2'
-				category={CategoryId.Photonvision}
-				name='Docs'
-				url='https://docs.photonvision.org/en/latest/'
+				title='AdvantageKit'
+				tiles={[{ name: 'Docs', url: 'https://github.com/Mechanical-Advantage/AdvantageKit#readme' }]}
 			/>
 
-			<TileSectionHeading className='bg-limelight bg-opacity-15 lg:col-start-3'>Limelight</TileSectionHeading>
-			<Tile
-				className='lg:col-start-3'
+			<TileGroup
+				category={CategoryId.Pathplanner}
+				className='lg:col-start-1'
+				title='PathPlanner'
+				tiles={[
+					{ name: 'Docs', url: 'https://pathplanner.dev/home.html' },
+					{ name: 'Examples', url: 'https://github.com/mjansen4857/pathplanner/tree/main/examples' },
+				]}
+			/>
+
+			<TileGroup
+				category={CategoryId.Photonvision}
+				className='lg:col-start-2'
+				title='Photonvision'
+				tiles={[{ name: 'Docs', url: 'https://docs.photonvision.org/en/latest/' }]}
+			/>
+
+			<TileGroup
 				category={CategoryId.Limelight}
-				name='Docs'
-				url='https://docs.limelightvision.io/'
+				className='lg:col-start-3'
+				title='Limelight'
+				tiles={[{ name: 'Docs', url: 'https://docs.limelightvision.io/' }]}
 			/>
 		</main>
 	);
--- src/app/tile-group.tsx
+++ src/app/tile-group.tsx
@@ -1,5 +1,43 @@
-import { PropsWithChildren } from 'react';
+import { CategoryId } from '@/content';
+import clsx from 'clsx';
+import { Tile } from './tile';
+import { TileSectionHeading } from './tile-section-heading';
 
-export function TileGroup({ children }: PropsWithChildren) {
-	return <section className='col-span-1 flex flex-col gap-4'>{children}</section>;
+type Entry = {
+	name: string;
+	url: string;
+};
+
+type Props = {
+	title: string;
+	category: CategoryId;
+	tiles: Entry[];
+	className: string;
+};
+
+export function TileGroup({ category, className, tiles, title }: Props) {
+	return (
+		<>
+			<TileSectionHeading
+				className={clsx(
+					'bg-opacity-15',
+					{
+						'bg-ctre': category === CategoryId.Ctre,
+						'bg-advantagekit': category === CategoryId.Advantagekit,
+						'bg-pathplanner': category === CategoryId.Pathplanner,
+						'bg-wpilib': category === CategoryId.Wpilib,
+						'bg-rev': category === CategoryId.Rev,
+						'bg-limelight': category === CategoryId.Limelight,
+						'bg-photonvision': category === CategoryId.Photonvision,
+					},
+					className,
+				)}
+			>
+				{title}
+			</TileSectionHeading>
+			{tiles.map(({ name, url }) => (
+				<Tile key={name} className={className} category={category} name={name} url={url} />
+			))}
+		</>
+	);
 }
