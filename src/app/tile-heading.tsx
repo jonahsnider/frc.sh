5be8daebd44c26825dc773e48abefed0f4d32ebc
Jonah Snider <jonah@jonahsnider.com>
2024-01-17 19:02:27 -0800

    feat: make rounding consistent in tile heading component

--- src/app/tile-heading.tsx
+++ src/app/tile-heading.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export function TileHeading({ children }: PropsWithChildren) {
 	return (
-		<div className='shadow px-2 py-4 col-span-full row-span-1 rounded transition-colors flex justify-center items-center bg-neutral-800 text-4xl'>
+		<div className='shadow px-2 py-4 col-span-full row-span-1 rounded-lg transition-colors flex justify-center items-center bg-neutral-800 text-4xl'>
 			{children}
 		</div>
 	);
