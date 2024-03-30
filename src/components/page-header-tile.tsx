6dfb9cbc600cc98daf9fa1a39c639a7ff9c033a4
Jonah Snider <jonah@jonahsnider.com>
2024-03-30 01:25:19 -0700

    feat: improve header tiles and add links to other projects

--- src/components/page-header-tile.tsx
+++ src/components/page-header-tile.tsx
@@ -1,30 +1,47 @@
-import { Code, Heading, Link, Text } from '@radix-ui/themes';
-import { TileHeading } from './tile-heading';
+import { Card, Code, Heading, Link, Text } from '@radix-ui/themes';
 import { Toggles } from './toggles';
 
 export function PageHeaderTile() {
 	return (
-		<TileHeading>
-			<div className='flex flex-col items-center justify-center gap-4'>
+		<>
+			<Card className='col-span-1 flex flex-col items-center justify-center gap-1'>
+				<Text as='p'>
+					Created by <Link href='https://jonahsnider.com'>Jonah Snider</Link>
+				</Text>
+
+				<Text as='p'>
+					View source on <Link href='https://github.com/jonahsnider/frc.sh'>GitHub</Link>
+				</Text>
+			</Card>
+
+			<Card className='col-span-1 row-start-1 md:col-span-2 sm:col-span-1 xs:col-span-2 sm:row-start-auto'>
 				<div className='flex flex-col items-center justify-center gap-1'>
 					<Heading as='h1' size='8'>
 						frc
-						<Code variant='ghost'>
-							<span className='-tracking-widest text-first-red'>.</span>
+						<Code variant='ghost' className='-tracking-widest'>
+							<span className='text-first-red'>.</span>
 							<span className='text-first-white'>s</span>
 							<span className='text-first-blue'>h</span>
 						</Code>
 					</Heading>
-					<Text as='p'>
-						Created by <Link href='https://jonahsnider.com'>Jonah Snider</Link>
-					</Text>
-					<Text as='p'>
-						View source on <Link href='https://github.com/jonahsnider/frc.sh'>GitHub</Link>
-					</Text>
-				</div>
 
-				<Toggles />
-			</div>
-		</TileHeading>
+					<Toggles />
+				</div>
+			</Card>
+
+			<Card className='col-span-1 flex flex-col items-center justify-center gap-1'>
+				<Heading as='h2' size='4'>
+					Other projects:
+				</Heading>
+
+				<Link href='https://scores.frc.sh/?utm_source=frc.sh' size='4'>
+					scores.frc.sh
+				</Link>
+
+				<Link href='https://frc-colors.com/?utm_source=frc.sh' size='4'>
+					FRC Colors
+				</Link>
+			</Card>
+		</>
 	);
 }
--- src/components/tile-heading.tsx
+++
@@ -1,6 +0,0 @@
-import { Card } from '@radix-ui/themes';
-import type { PropsWithChildren } from 'react';
-
-export function TileHeading({ children }: PropsWithChildren) {
-	return <Card className='col-span-full'>{children}</Card>;
-}
